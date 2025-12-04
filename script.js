42f27807eaabdf335b0ef36bfde2a8a7e0cb414f
Abhishek <135864521+abhis20@users.noreply.github.com>
2025-12-04 22:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const modal = document.getElementById("myModal");
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.querySelector(".close-modal");
 
+// Open modal
 openBtn.addEventListener("click", () => {
-  modal.style.display = "flex";
+  modal.style.display = "flex";   // show modal centered
 });
 
+// Close modal with X button
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
+// Close modal when clicking outside modal-content
 window.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
